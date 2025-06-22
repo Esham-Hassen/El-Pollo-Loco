c42582e1198a91d52f259a7829e179a772ac6242
Esham-Hassen <eshamhassen31@gmail.com>
2025-06-22 23:23:42 +0200

    Added backgroundObject

--- models/levels/level1.js
+++ models/levels/level1.js
@@ -22,7 +22,7 @@ function initLevel() {
             new Coin(1500, 220)
         ],
 
-         [
+        [
             new Bottle(200, 350),
             new Bottle(500, 355),
             new Bottle(800, 357),
@@ -62,11 +62,24 @@ function initLevel() {
             new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
             new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
             new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
+
+
+            new BackgroundObject('img/5_background/layers/air.png', 719 * 4),
+            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 4),
+            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 4),
+            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 4),
+
+            new BackgroundObject('img/5_background/layers/air.png', 719 * 5),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 5),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 5),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 5),
+
         ],
 
 
 
-       
+
+
 
 
     )
